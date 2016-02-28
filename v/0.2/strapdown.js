04e74bd1670cabc0c4f6306b52cffff50d515452
jamesb <me@localhost>
2016-02-28 20:36:40 +1100

    use <textarea> instead of <pre> to preserve inline HTML

--- v/0.2/strapdown.js
+++ v/0.2/strapdown.js
@@ -338,8 +338,7 @@ var PR=win['PR']={'createSimpleLexer':createSimpleLexer,'registerLangHandler':re
   // Get user elements we need
   //
 
-  var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0] ||
-                   document.getElementsByTagName('pre')[0],
+  var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0],
       titleEl = document.getElementsByTagName('title')[0],
       scriptEls = document.getElementsByTagName('script'),
       navbarEl = document.getElementsByClassName('navbar')[0];
