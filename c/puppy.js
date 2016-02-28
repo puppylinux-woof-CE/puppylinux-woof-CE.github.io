3518c0fe943de17b76c5d465f74ca570afc1f3c6
jamesb <me@localhost>
2016-02-28 19:09:42 +1100

    get rid of div header/footer, puppy.js now handles that. Also fixes the "double-navbar" problem.

--- c/puppy.js
+++ c/puppy.js
@@ -1,8 +1,20 @@
 /* Puppy specific stuff */
 
+// header and footer
+// create header first - important so strapdown.js won't attempt to create its own
+var header = document.createElement('div');
+$(header).addClass("navbar"); // this is what strapdown.js is looking for
+document.body.insertBefore(header, document.body.firstChild);
+$().get("c/header.html", function(data) { 
+	$(header).html(data);
+});
+
 // load header and footer
-$().get("c/header.html", function(data) { $("#header").html(data) });
-$().get("c/footer.html", function(data) { $("#footer").html(data) });
+$().get("c/footer.html", function(data) { 
+	var e = document.createElement('div');
+	$(e).html(data);
+	document.body.appendChild(e);
+});
 
 // stylesheets
 var e = document.createElement('link');
